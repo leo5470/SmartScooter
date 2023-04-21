8c332b8845ce5ca333ecab9afbbccaf67a278154
andre.liang <54308739+namwoam@users.noreply.github.com>
2023-04-21 12:02:44 +0800

    init a spring boot project

---
+++ smart-scooter/src/main/java/com/esoe2013group1/smartscooter/SmartScooterApplication.java
@@ -0,0 +1,22 @@
+package com.esoe2013group1.smartscooter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SmartScooterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartScooterApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		String description = new String(getClass().getPackageName());
+		return description;
+	}
+
+}
---
+++ smart-scooter/src/test/java/com/esoe2013group1/smartscooter/SmartScooterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esoe2013group1.smartscooter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartScooterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
