3d7e5d06f1998dffc75c68aa41b0b603a5c55f77
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-05-12 19:53:06 +0800

    Update AuthFailedException.java

--- smart-scooter/src/main/java/com/esoe2013group1/smartscooter/exception/AuthFailedException.java
+++ smart-scooter/src/main/java/com/esoe2013group1/smartscooter/exception/AuthFailedException.java
@@ -2,6 +2,6 @@ package com.esoe2013group1.smartscooter.exception;
 
 public class AuthFailedException extends Exception{
     public AuthFailedException(){
-        super("Auth failed.");
+        super("Authentication failed. Please check your password and try again.");
     }
 }
