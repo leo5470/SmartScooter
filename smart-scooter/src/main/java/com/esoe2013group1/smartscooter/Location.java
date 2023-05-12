c40346e9fefd465d75cfb03de2e94305ef33dd40
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-05-12 19:53:14 +0800

    Update Location.java

--- smart-scooter/src/main/java/com/esoe2013group1/smartscooter/Location.java
+++ smart-scooter/src/main/java/com/esoe2013group1/smartscooter/Location.java
@@ -1,6 +1,13 @@
 package com.esoe2013group1.smartscooter;
 
+import java.util.Random;
+
 public class Location {
+    public static final double latMax = 25.068277;
+    public static final double latMin = 25.026708;
+    public static final double lngMax = 121.567045;
+    public static final double lngMin = 121.511162;
+
     private double lat;
     private double lng;
 
@@ -18,5 +25,11 @@ public class Location {
 
     public void setLng(double lng) {
         this.lng = lng;
+    }
+
+    public Location(){
+        Random r = new Random();
+        lat = latMin + (latMax - latMin) * r.nextDouble();
+        lng = lngMin + (lngMax - lngMin) * r.nextDouble();
     }
 }
