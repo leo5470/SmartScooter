bd48f4e0491a5a0dd1fc0d1b5f54ded763faab42
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-05-20 10:44:56 +0800

    bug fixes

--- smart-scooter/src/main/java/com/esoe2013group1/smartscooter/entity/Scooter.java
+++ smart-scooter/src/main/java/com/esoe2013group1/smartscooter/entity/Scooter.java
@@ -25,4 +25,52 @@ public class Scooter {
     private double lng;
     @Column(nullable = false, length = 16)
     private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public Integer getBattery_level() {
+        return battery_level;
+    }
+
+    public void setBattery_level(Integer battery_level) {
+        this.battery_level = battery_level;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
