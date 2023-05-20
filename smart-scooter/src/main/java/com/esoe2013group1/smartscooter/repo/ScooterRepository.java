de7e49cf29f18e0a9b1554895497d0c817791d8d
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-05-20 10:45:04 +0800

    Update ScooterRepository.java

--- smart-scooter/src/main/java/com/esoe2013group1/smartscooter/repo/ScooterRepository.java
+++ smart-scooter/src/main/java/com/esoe2013group1/smartscooter/repo/ScooterRepository.java
@@ -4,7 +4,10 @@ import com.esoe2013group1.smartscooter.entity.Scooter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScooterRepository
         extends JpaRepository<Scooter, Integer> {
+    List<Scooter> findAllByLatBetweenAndLngBetween(double minLat, double maxLat, double minLng, double maxLng);
 }
