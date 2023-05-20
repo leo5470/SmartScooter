e60817252fd00273bfef628f6bf7fbd53c9b0d77
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-05-20 10:44:38 +0800

    Create ScooterID.java

---
+++ smart-scooter/src/main/java/com/esoe2013group1/smartscooter/ScooterID.java
@@ -0,0 +1,14 @@
+package com.esoe2013group1.smartscooter;
+
+public class ScooterID {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
