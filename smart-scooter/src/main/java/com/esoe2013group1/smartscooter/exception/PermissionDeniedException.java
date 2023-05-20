0dced245c6dea81778fb027800db715adc38fca2
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-05-20 16:08:12 +0800

    Update PermissionDeniedException.java

--- smart-scooter/src/main/java/com/esoe2013group1/smartscooter/exception/PermissionDeniedException.java
+++ smart-scooter/src/main/java/com/esoe2013group1/smartscooter/exception/PermissionDeniedException.java
@@ -1,5 +1,7 @@
 package com.esoe2013group1.smartscooter.exception;
 
 public class PermissionDeniedException extends Exception{
-
+    public PermissionDeniedException(){
+        super("Permission denied.");
+    }
 }
