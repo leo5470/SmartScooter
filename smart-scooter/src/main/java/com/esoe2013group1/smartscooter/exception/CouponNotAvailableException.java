a474d5047efd84b1cd33f3e5726391b4a60c7041
leo5470 <61446148+leo5470@users.noreply.github.com>
2023-06-14 15:53:00 +0800

    YET ANOTHER EXCEPTION

---
+++ smart-scooter/src/main/java/com/esoe2013group1/smartscooter/exception/CouponNotAvailableException.java
@@ -0,0 +1,7 @@
+package com.esoe2013group1.smartscooter.exception;
+
+public class CouponNotAvailableException extends Exception{
+    public CouponNotAvailableException(){
+        super("Coupon not available.");
+    }
+}
