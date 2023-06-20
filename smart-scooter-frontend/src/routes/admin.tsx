bf2c6935f7fc6f7b60dcf84f5f9cfb7e04cb12a9
andre.liang <54308739+namwoam@users.noreply.github.com>
2023-06-20 15:01:40 +0800

    no is_admin qq

--- smart-scooter-frontend/src/routes/admin.tsx
+++ smart-scooter-frontend/src/routes/admin.tsx
@@ -1,14 +1,17 @@
 import { useOutlet } from "react-router-dom";
 import AdminNav from "../component/admin-nav"
 import Showcase from "../component/showcase";
-
-
+import { atom_data } from "../lib/store";
+import { useAtom } from "jotai";
+import { Navigate } from "react-router-dom";
 export default function Admin() {
     const outlet = useOutlet();
+    const [data , _] = useAtom(atom_data)
     return (
         <>
             <AdminNav></AdminNav>
             <main className="container">
+            {data.current_user.id == -1? <Navigate to="/"/>:""}
                 {outlet || <Showcase modelLocation={[-0.5, -2, 0]} modelRotation={[0, Math.PI * (1.65), -0.05]} modelScale={30} text="Start Working!" modelName="terminal" textLocation={[-3.8, -0.4, 3]}></Showcase>}
             </main>
         </>
