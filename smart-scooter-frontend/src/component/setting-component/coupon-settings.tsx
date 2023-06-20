a11f72bacb04c8d1be41b1eb99ba8cede199d7a9
andre.liang <54308739+namwoam@users.noreply.github.com>
2023-06-20 14:01:35 +0800

    Update coupon-settings.tsx

--- smart-scooter-frontend/src/component/setting-component/coupon-settings.tsx
+++ smart-scooter-frontend/src/component/setting-component/coupon-settings.tsx
@@ -1,37 +1,40 @@
-import { useState } from "react";
-
+import { useAtom } from "jotai"
+import { atom_data } from "../../lib/store"
+import { useEffect, useState } from "react"
 export default function CouponSettings() {
-    const [showModal, setShowModal] = useState(false);
-
-
-    const toggleModal = () => {
-        setShowModal(!showModal);
-    };
+    const [data, _] = useAtom(atom_data)
+    const [coupon_array, set_coupon_array] = useState<Array<null>>(new Array(data.current_user.coupons));
+    useEffect(() => {
+        const coupons = []
+        for (let i=0;i<data.current_user.coupons;i++){
+            coupons.push(null)
+        }
+        set_coupon_array(coupons)
+    } , [data.current_user.coupons])
     return (
         <div>
             <article>
                 <hgroup>
-                <h1>Coupon Settings</h1>
-                <h2></h2>
+                    <h1>Your coupons ({data.current_user.coupons})</h1>
+                    <h2></h2>
                 </hgroup>
-            <select>
-                <option value="time">Sort by Time</option>
-                <option value="discount">Sort by Discount</option>
-            </select>
+                <select>
+                    <option value="discount">Sort by Discount</option>
+                </select>
 
-            <div className="coupon-list">
-                {/* Example coupon */}
-                <div className="coupon-item">
-                    <div className="coupon-details">
-                        <h3>Coupon Title</h3>
-                        <p>Discount: 10%</p>
-                        <p>Due Date: 2023-06-30</p>
-                    </div>
-                    <button className="contrast" onClick={toggleModal}>
-                        View Details
-                    </button>
+                <div className="coupon-list">
+                    {coupon_array.map(() => {
+                        return (<>
+                            <div className="coupon-item">
+                                <div className="coupon-details">
+                                    <h3>Normal Coupon</h3>
+                                    <p>Discount: 10%</p>
+                                </div>
+                            </div>
+                        </>)
+                    })}
+
                 </div>
-            </div>
             </article>
         </div>
     )
